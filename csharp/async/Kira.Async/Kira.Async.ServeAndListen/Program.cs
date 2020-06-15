679173ed6cb973e2767f18335d14e109974563b5
Kirill Lappo <kirill-lappo@outlook.com>
2020-06-15 18:45:22 +0300

    init commit

---
+++ csharp/async/Kira.Async/Kira.Async.ServeAndListen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace Kira.Async.ServeAndListen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartServerAsync();
+        }
+
+        private static Task StartServerAsync()
+        {
+            return Task.Run(() =>
+            {
+                var server = new Server("127.0.0.1", 8888);
+                server.StartServe();
+            });
+        }
+    }
+}
---
+++ csharp/async/Kira.Async/Kira.Async.ServeAndListen/Server.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Kira.Async.ServeAndListen
+{
+    public class Server
+    {
+        public int Port { get; }
+
+        public string Host { get; }
+
+        public Server(string host, int port)
+        {
+            Host = host;
+            Port = port;
+        }
+
+        public void StartServe()
+        {
+            var listener = CreateTcpListener();
+
+            listener.Start();
+            try
+            {
+                HandleClientRequests(listener);
+            }
+            finally
+            {
+                listener?.Stop();
+            }
+        }
+
+        private void HandleClientRequests(TcpListener listener)
+        {
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var clientStream = client.GetStream();
+
+                try
+                {
+                    if (!TryHandleStream(clientStream))
+                    {
+                        break;
+                    }
+                }
+                finally
+                {
+                    clientStream?.Close();
+                    client?.Close();
+                }
+            }
+        }
+
+        private bool TryHandleStream(NetworkStream clientStream)
+        {
+            var command = ReadCommand(clientStream)?.ToLower();
+
+            switch (command)
+            {
+                case null:
+                    Console.WriteLine("Can't read command from client");
+                    break;
+
+                case "exit":
+                    Console.WriteLine("Shutting down the  server...");
+                    return false;
+
+                default:
+                    Console.WriteLine("Incoming command: " + command);
+                    break;
+            }
+
+            return true;
+        }
+
+        private string ReadCommand(NetworkStream stream)
+        {
+            var buffer = new Span<byte>();
+            stream.Read(buffer);
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        private TcpListener CreateTcpListener()
+        {
+            return new TcpListener(IPAddress.Parse(Host), Port);
+        }
+    }
+}
